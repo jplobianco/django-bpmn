e22cc3aaac9502ec193ad1f680ba1a1de6e151e0
Joao Paulo Lobianco Silva <joaopaulolobianco1@gmail.com>
2019-05-13 21:47:46 -0300

    Reestrutura e renomeia as pastas do projeto

--- django-bpmn/wsgi.py
+++ django-bpmn/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_bpmn_project project.
+WSGI config for django-bpmn project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_bpmn_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django-bpmn.settings")
 
 application = get_wsgi_application()
